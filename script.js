c69cdaa3db267e801336066f61f1a0c6bfb90434
marc17x <marc20964@gmail.com>
2022-08-24 21:36:19 +0200

    darkmode button

---
+++ script.js
@@ -0,0 +1,6 @@
+/*dark_mode js function*/
+const dark_mode_button = document.getElementById("dark_mode_button")
+dark_mode_button.addEventListener("click", () =>{
+    document.body.classList.toggle("dark_mode")
+}) 
+/*dark_mode js function END */
